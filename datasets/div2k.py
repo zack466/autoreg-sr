f7d6875518df7d6324ce5c82f1defdda854bfab2
zack466 <zack466@gmail.com>
2020-12-06 21:40:12 -0500

    using black for formatting

--- datasets/div2k.py
+++ datasets/div2k.py
@@ -11,13 +11,14 @@ import os
 import numpy as np
 from .cache import HDF5Cache
 
+
 class Div2KTiny(Dataset):
     def __init__(self, device):
         ## super init??
-        self.data_path = os.path.dirname(__file__)+"/saved/div2k/train-x2/"
+        self.data_path = os.path.dirname(__file__) + "/saved/div2k/train-x2/"
         self.trainsets = os.listdir(self.data_path)[:-1]
         self.device = device
-        self.current = (None, None) # pointer to images and labels
+        self.current = (None, None)  # pointer to images and labels
         self.load_set(0)
 
     def __len__(self):
@@ -25,19 +26,24 @@ class Div2KTiny(Dataset):
 
     def load_set(self, set):
         self.set_number = set
-        with h5py.File(self.data_path + self.trainsets[set], 'r') as f:
-             # for some reason h5 --> np.array --> torch.tensor is much faster than h5 --> torch.tensor
-             images = torch.tensor(np.array(f['images'])).permute(0,3,1,2).to(self.device)
-             labels = torch.tensor(np.array(f['labels'])).permute(0,3,1,2).to(self.device)
-             self.current = (images, labels)
+        with h5py.File(self.data_path + self.trainsets[set], "r") as f:
+            # for some reason h5 --> np.array --> torch.tensor is much faster than h5 --> torch.tensor
+            images = (
+                torch.tensor(np.array(f["images"])).permute(0, 3, 1, 2).to(self.device)
+            )
+            labels = (
+                torch.tensor(np.array(f["labels"])).permute(0, 3, 1, 2).to(self.device)
+            )
+            self.current = (images, labels)
 
     def __getitem__(self, idx):
-        if idx // 4096 == self.set_number: # should already be open
+        if idx // 4096 == self.set_number:  # should already be open
             pass
         else:
             self.load_set(idx // 4096)
-        sample = {'image':self.current[0][idx], 'label':self.current[1][idx]}
+        sample = {"image": self.current[0][idx], "label": self.current[1][idx]}
         return sample
+
 
 class Div2K(Dataset):
     def __init__(self, size, factor):
@@ -47,7 +53,7 @@ class Div2K(Dataset):
         """
         self.size = size
         self.factor = factor
-        self.cache = HDF5Cache('div2k-x2', 16)
+        self.cache = HDF5Cache("div2k-x2", 16)
 
     def __len__(self):
         return 800
@@ -55,26 +61,32 @@ class Div2K(Dataset):
     def __getitem__(self, idx):
         # TODO: what is behavior if idx is > 800?
         return self.cache.get_item(idx, self)
-    
+
     def cache_func(self, i):
         # caches the ith chunk of images
         lr_images = []
         hr_images = []
-        offset = i*self.cache.cache_size
+        offset = i * self.cache.cache_size
         for idx in range(self.cache.cache_size):
-            img_hr_name = "./datasets/saved/DIV2K_train_HR/" + str(offset+idx+1).zfill(4) + ".png"
-            img_lr_name = f"./datasets/saved/DIV2K_train_LR_bicubic/X{self.factor}/" + str(offset+idx+1).zfill(4) + f"x{self.factor}.png"
+            img_hr_name = (
+                "./datasets/saved/DIV2K_train_HR/"
+                + str(offset + idx + 1).zfill(4)
+                + ".png"
+            )
+            img_lr_name = (
+                f"./datasets/saved/DIV2K_train_LR_bicubic/X{self.factor}/"
+                + str(offset + idx + 1).zfill(4)
+                + f"x{self.factor}.png"
+            )
             # C,H,W
             img_hr = Image.open(img_hr_name)
             img_lr = Image.open(img_lr_name)
-            hr_transform = transforms.Compose([
-                transforms.CenterCrop(self.size * self.factor),
-                transforms.ToTensor()
-            ])
-            lr_transform = transforms.Compose([
-                transforms.CenterCrop(self.size),
-                transforms.ToTensor()
-            ])
+            hr_transform = transforms.Compose(
+                [transforms.CenterCrop(self.size * self.factor), transforms.ToTensor()]
+            )
+            lr_transform = transforms.Compose(
+                [transforms.CenterCrop(self.size), transforms.ToTensor()]
+            )
             lr_images.append(lr_transform(img_lr))
             hr_images.append(hr_transform(img_hr))
         lr_stacked = np.stack(lr_images)
@@ -83,5 +95,3 @@ class Div2K(Dataset):
         lr_type = lr_stacked.astype(np.float32)
         hr_type = hr_stacked.astype(np.float32)
         self.cache.cache_images(i, lr_type, hr_type)
-    
-    
--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,7 @@ import torchvision
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 def showImage(img):
     # expects ndarray with shape CHW
 
@@ -11,12 +12,13 @@ def showImage(img):
     # torchvision.transforms.ToPILImage()((img * 256).astype('uint8')).show()
     # torchvision.transforms.ToPILImage()(img).show()
 
-    if 0. < img[0][0][0] < 1.: # hacky way of checking for float32
-        plt.imshow((img* 256).astype('uint8').transpose(2,1,0))
+    if 0.0 < img[0][0][0] < 1.0:  # hacky way of checking for float32
+        plt.imshow((img * 256).astype("uint8").transpose(2, 1, 0))
+
 
 def showImages(imgs):
     fig = plt.figure()
     for i, image in enumerate(imgs):
-        fig.add_subplot(1, len(imgs), i+1)
-        plt.imshow((image* 256).astype('uint8').transpose(2,1,0))
+        fig.add_subplot(1, len(imgs), i + 1)
+        plt.imshow((image * 256).astype("uint8").transpose(2, 1, 0))
     plt.show()
