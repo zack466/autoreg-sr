88146370c04bc299c0f4fa3a43d9dbc237bb102c
zack466 <zack466@gmail.com>
2021-02-03 01:56:46 -0500

    etc

--- overnight.sh
+++ overnight.sh
@@ -6,7 +6,8 @@
 # python train.py --model ConvSR --loss VGG16Partial --ckpt_name ConvSR_32_final --lr_size 32 --batch_size 64 --num_patches 32 --epochs 20 --metrics PSNR SSIM consistency sample lr --pre_upscale True
 
 python inference.py --model PartialSR --ckpt_name PartialSR_32_final --lr_size 32 --batch_size 64 --num_patches 32 --img_path ./0002x4.png --patches True
-python inference.py --model ConvSR --ckpt_name ConvSR_32_final --lr_size 32 --batch_size 64 --num_patches 32 --pre_upscale True --img_path ./0002x4.png --patches True
+python inference.py --model ConvSR --ckpt_name ConvSR_32_final --lr_size 32 --batch_size 64 --num_patches 32 --pre_upscale True --patches True --separate True
+python inference.py --model SRCNN --ckpt_name SRCNN_final --lr_size 32 --batch_size 64 --num_patches 32 --pre_upscale True --patches True --separate True
 
 python train.py --model SRCNN --loss psnr_loss --ckpt_name SRCNN_classic --lr_size 16 --batch_size 32 --num_patches 4 --epochs 1 --metrics PSNR SSIM lr sample VGG16Partial --pre_upscale True
 
